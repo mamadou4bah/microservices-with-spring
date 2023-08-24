99bfc78a337e8dcc3f9c615a646b28890b078b7d
Mamadou BAH <mamadou.bah@natan.fr>
2023-08-24 14:45:06 +0200

    Implements gatewayserver and cross cutting concerns

---
+++ section2-to-11/gatewayserver/src/main/java/com/mamadou4bah/gatewayserver/GatewayserverApplication.java
@@ -0,0 +1,39 @@
+package com.mamadou4bah.gatewayserver;
+
+import java.util.Date;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+	
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+	    return builder.routes()
+	        .route(p -> p
+	            .path("/mbabank/accounts/**")
+	            .filters(f -> f.rewritePath("/mbabank/accounts/(?<segment>.*)","/${segment}")
+	            				.addResponseHeader("X-Response-Time",new Date().toString()))
+	            .uri("lb://ACCOUNTS"))
+				.route(p -> p
+		            .path("/mbabank/loans/**")
+		            .filters(f -> f.rewritePath("/mbabank/loans/(?<segment>.*)","/${segment}")
+		            		.addResponseHeader("X-Response-Time",new Date().toString()))
+		            .uri("lb://LOANS"))
+				.route(p -> p
+		            .path("/mbabank/cards/**")
+		            .filters(f -> f.rewritePath("/mbabank/cards/(?<segment>.*)","/${segment}")
+		            		.addResponseHeader("X-Response-Time",new Date().toString()))
+		            .uri("lb://CARDS"))
+				.build();
+	}
+
+}
